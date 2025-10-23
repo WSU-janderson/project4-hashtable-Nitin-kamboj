18638be4d3047bba60280e84ee39423fc61a1ea8
Nitin-kamboj <nitinmirok201@gmail.com>
2025-10-23 14:07:53 -0400

    Created methods in HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,3 +1,38 @@
 /**
  * HashTable.cpp
  */
+#include "HashTable.h"
+# include <iostream>
+using namespace std;
+    HashTable::HashTable(size_t capcity) {
+        this->capcity = capcity;
+    }
+bool HashTable::insert(string key, int value) {
+
+    }
+bool HashTable::remove(string key) {
+
+}
+bool HashTable::contains(string key) {
+
+    }
+int HashTable::get(string key) {
+
+    }
+int HashTable::operator[](string key) {
+
+}
+vector<string> HashTable::keys() {
+
+}
+double HashTable::alpha() {
+
+}
+size_t HashTable::capacity() {
+
+}
+
+size_t HashTable::size() {
+
+    }
+
